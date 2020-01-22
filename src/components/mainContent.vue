8a10a8d4142a8c53e9a470ced8d2fe3422772db1
Taiki Ogawa <ogawa_taiki@sinap.jp>
2020-01-22 11:42:41 +0900

    基本のパーツ追加

---
+++ src/components/mainContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="wrapper">
+    <h1 class="heading1">タスクを始める</h1>
+    <p>入力済みタスクは後ほど編集できます。</p>
+    <input type="text" />
+    <button>追加</button>
+    <button>クリア</button>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "mainContent",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
