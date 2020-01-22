6e4d3224f17620f57bb99ab30d3657204ff0a74c
Taiki Ogawa <ogawa_taiki@sinap.jp>
2020-01-22 11:56:40 +0900

    testがコケたので調整

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import mainContent from "@/components/mainContent.vue";
 
-describe("HelloWorld.vue", () => {
+describe("mainContent.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(mainContent, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
