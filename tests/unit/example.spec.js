5bc6681bf985eea165e0708f61a153c6c8011b70
Taiki Ogawa <ogawa_taiki@sinap.jp>
2020-02-05 10:48:44 +0900

    空だとエラーになるので適当なテストを追加

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,3 @@
-// import { shallowMount } from "@vue/test-utils";
-// import { mainContent } from "@/components/mainContent.vue";
-
-// describe("mainContent.vue", () => {
-//   it("renders props.msg when passed", () => {
-//     const msg = "new message";
-//     const wrapper = shallowMount(mainContent, {
-//       propsData: { msg }
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
-// });
+describe("example", () => {
+  it("example");
+});
