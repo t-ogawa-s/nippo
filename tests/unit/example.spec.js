69f20653683b2286389d7c73531792d336883c3a
Taiki Ogawa <ogawa_taiki@sinap.jp>
2020-02-05 10:42:54 +0900

    不要なテストを一時コメントアウト

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from "@vue/test-utils";
-import mainContent from "@/components/mainContent.vue";
+// import { shallowMount } from "@vue/test-utils";
+// import { mainContent } from "@/components/mainContent.vue";
 
-describe("mainContent.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(mainContent, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+// describe("mainContent.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(mainContent, {
+//       propsData: { msg }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
